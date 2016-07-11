2fb46b9255558a5d13af23cbe992fc3fe2ccdc0a
Luciano Serafim de Souza <lucianoserafimdesouza@gmail.com>
2016-07-11 13:01:13 -0300

    Commit direto. Sem conseguir pelo RStudio

---
+++ psiniw.R
@@ -0,0 +1,29 @@
+#' The cumulative function of the SinInverseWeibull probability distribution.
+#'
+#' @param q vector of quantiles.
+#' @param alpha Alpha parameter.
+#' @param theta Theta parameter.
+#' @param lower Lower parameter.
+#' @param log.p Log.p parameter.
+#' @return A vector with n observations of the SinInverseWeibull distribution.
+#' @examples
+#' psiniw(q,1,1,TRUE,FALSE)
+#' psiniw(q,0.5,0.7,TRUE,FALSE)
+
+psiniw <- function(q,alpha,theta,lower = TRUE,log.p = FALSE){
+
+  if (log.p == TRUE) {
+    if (lower == TRUE){
+      log(sin((pi/2)*exp(-alpha*q^(-theta))))
+    }else{
+      log((1 - sin((pi/2)*exp(-alpha*q^(-theta)))))
+    }
+  } else {
+    if (lower == TRUE){
+      sin((pi/2)*exp(-alpha*q^(-theta)))
+    }else{
+      (1 - sin((pi/2)*exp(-alpha*q^(-theta))))
+    }
+  }
+
+}
